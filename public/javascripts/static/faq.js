75ed6272394ee8e319ec69caf794644a0b0c88e9
Josh Silverman <joshs.silverman@gmail.com>
2011-06-11 00:38:24 -0400

    FAQ and new styling for static pages

--- public/javascripts/static/faq.js
+++ public/javascripts/static/faq.js
@@ -6,35 +6,53 @@ var cDoc = Class.create({
         /* resize listener */
         window.onresize = AppUtilities.resizeContents;
         AppUtilities.resizeContents();
-        
+
+
         this.toggleSwitch = $('toggle_switch');
+
+        $$('.heading span').each(function(heading) {
+            heading.observe('click', this.toggleFact.curry(heading.up('.fact-wrapper'), null));
+        }.bind(this));
+    },
+
+    toggleFact: function(factWrapper, collapsed) {
+        var fact = factWrapper.select('.fact')
+        var heading = factWrapper.select('.heading span')
+        if (fact.length > 0 && heading.length > 0) {
+            fact = fact[0];
+            heading = heading[0];
+        }
+        else {
+            console.log("couldn't find elements");
+            return;
+        }
+
+
+        if (fact.collapsed == 'false' && collapsed === null || collapsed) {
+            fact.hide();
+            heading.removeClassName("expanded");
+            fact.collapsed = "true"
+        }
+        else {
+            fact.show();
+            heading.addClassName("expanded");
+            fact.collapsed = "false"
+        }
     },
 
     toggleFacts: function() {
-        console.log(this.toggleSwitch);
-        console.log(this.toggleSwitch.collapsed);
+
         if (this.toggleSwitch.collapsed == 'false') {
-            console.log("1");
-            $$(".fact").each(function(fact) {
-                fact.hide();
-            });
-            $$(".heading").each(function(fact) {
-                fact.removeClassName("expanded");
-            });
             this.toggleSwitch.collapsed = "true"
             this.toggleSwitch.update("Expand All");
         }
         else {
-            console.log(this.toggleSwitch);
-            $$(".fact").each(function(fact) {
-                fact.show();
-            });
-            $$(".heading").each(function(fact) {
-                fact.addClassName("expanded");
-            });
             this.toggleSwitch.collapsed = "false"
             this.toggleSwitch.update("Collapse All");
         }
+        $$(".fact-wrapper").each(function(factWrapper) {
+            this.toggleFact(factWrapper, this.toggleSwitch.collapsed == 'true');
+        }.bind(this));
     }
 });
 
