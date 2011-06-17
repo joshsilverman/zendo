79ea5a4fe3f721b9c04654143918d399c606c5a8
Josh Silverman <joshs.silverman@gmail.com>
2011-06-17 11:59:25 -0400

    new folder infrastructure part 5

--- public/javascripts/class_selector.js
+++ public/javascripts/class_selector.js
@@ -18,8 +18,8 @@ var cClassSelector = Class.create({
             if (option._selected == 'true') option.selected = true;
         });
 
-//        new Dialog.Box('new_folder_option');
-//        console.log($('new_folder_option'));
+        new Dialog.Box('new_folder_option');
+        $('new_folder_option').show();
     },
 
     changeClass: function(event) {
