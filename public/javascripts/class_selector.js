7b459f0999cad40f54c6526ca0fe2825dfbae327
bderusha <bill.derusha@gmail.com>
2011-06-16 21:55:09 -0400

    new dash

--- public/javascripts/class_selector.js
+++ public/javascripts/class_selector.js
@@ -1,11 +1,15 @@
 var cClassSelector = Class.create({
+    docId:null,
 
-    initialize: function() {
+    initialize: function(docId) {
+        this.docId = (typeof docId == "undefined")? false : docId;
         /* collect class options */
         var selector = $$('#tag_id')[0];
         if (selector) {
             console.log(selector);
             selector.observe('change', function(element) {
+                console.log(selector);
+                console.log(element.target);
                 this.changeClass(element);
             }.bind(this));
         }
@@ -18,7 +22,13 @@ var cClassSelector = Class.create({
     changeClass: function(element) {
 
         var parameters = {};
-        parameters['doc_id'] = $('doc_id').innerHTML;
+        if(!this.docId){
+           parameters['doc_id'] = $('doc_id').innerHTML;
+           console.log('1st: '+parameters['doc_id']);
+        } else {
+           parameters['doc_id'] =  $('metainfo').getAttribute('doc_id');
+           console.log('2nd: '+parameters['doc_id']);
+        }
         var selected = element.target.select("option[selected]");
         if (selected.length > 0) parameters['tag_id'] = selected[0].value;
         else {
