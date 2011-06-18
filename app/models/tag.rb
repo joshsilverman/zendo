2a7267371af41f8123d896783ceddb41201d96c9
Josh Silverman <joshs.silverman@gmail.com>
2011-06-17 21:25:13 -0400

    enforce 40 char limit on tag.name and document.name on frontent ; changing title directly after load dirties document

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,8 +1,8 @@
 class Tag < ActiveRecord::Base
   
   validates :name, :length => {:minimum => 1,
-                               :maximum => 30,
-                               :message => "Name must be between 1-30 characters"},
+                               :maximum => 40,
+                               :message => "Name must be between 1-40 characters"},
                    :format => {:with => /[a-zA-Z0-9-&%$\#+\(\)*^@!.]/,
                                :message => "Please use only letters numbers and (!@#\$%^&*-+)"}
   
