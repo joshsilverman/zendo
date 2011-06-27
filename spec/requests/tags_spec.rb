68c5a2e9b0162a05f82bf15abfaa75cde6d4abf3
bderusha <bill.derusha@gmail.com>
2011-06-26 22:13:00 -0400

    tags spec

--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -1,25 +1,36 @@
 require "spec_helper"
 
-describe "tags" do
+describe "tags", :js => true do
 
   before(:each) do
+    if example.metadata[:js]
+      Capybara.current_driver = :selenium
+      Capybara.default_wait_time = 3
+    else
+      Capybara.current_driver = :rack_test
+    end
+  end
 
-    Capybara.current_driver = :selenium if example.metadata[:js]
+  describe "nagivation", :js => true do
 
+    before(:each) do
     @user = Factory.create(:user)
     @user.save!
     visit "/users/sign_in"
     fill_in "Email", :with => @user.email
     fill_in "Password", :with => @user.password
     click_button "Sign in"
+
+    @tag = Factory.create(:tag, :user_id => @user.id)
+    @tag.save!
   end
 
-  describe "nagivation" do
-
     it "goes to Misc folder after click Misc", :js => true do
-#      click_on "Misc."
-#      page.should have_content "My Notes /Misc./"
+      visit "/explore"
+      wait_until{ page.has_content?('Misc.')}
+      find('div.accordion_toggle', :text => 'Misc.').click
+      wait_until{ page.has_content?('Saved')}
     end
- 
+
   end
 end
