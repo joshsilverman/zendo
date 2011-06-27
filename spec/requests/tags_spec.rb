510a3f8e7b69575f8b3520ef6e05451e61e473f4
bderusha <bill.derusha@gmail.com>
2011-06-26 22:11:32 -0400

    working tag spec

--- spec/requests/tags_spec.rb
+++ spec/requests/tags_spec.rb
@@ -5,7 +5,7 @@ describe "tags", :js => true do
   before(:each) do
     if example.metadata[:js]
       Capybara.current_driver = :selenium
-      Capybara.default_wait_time = 5
+      Capybara.default_wait_time = 3
     else
       Capybara.current_driver = :rack_test
     end
