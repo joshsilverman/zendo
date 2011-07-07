51d1ce296745b86dadc82bdc575ef1785f6c10e9
Josh Silverman <joshs.silverman@gmail.com>
2011-07-06 22:07:07 -0400

    lookup function for bold/autodef

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'rails', '3.0.3'
 gem 'mysql2', '0.2.7'
 
 gem 'nokogiri'
+gem 'scrapi', '1.2.0'
 
 gem 'devise'
 gem 'omniauth', '0.2.2'
--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -85,13 +85,6 @@ class DocumentsController < ApplicationController
     render :json => Tag.tags_json(current_user)
   end
 
-
-
-
-
-
-
-
   def review
 
     get_document(params[:id])
@@ -135,15 +128,6 @@ class DocumentsController < ApplicationController
     end
 	
   end
-
-
-
-
-
-
-
-
-
 
   def update_tag
     if @document = current_user.documents.find(params[:doc_id])
@@ -208,6 +192,35 @@ class DocumentsController < ApplicationController
     end
     render :nothing => true, :status => 400
 
+  end
+
+  def lookup
+    begin
+      description_index = nil
+      wiki_article = Scraper.define do
+        array :description
+        array :image
+        i = 0
+        process "#bodyContent >p", :description=>:text do |element|          
+          description_index = i if (element.to_s =~ /^<p[^>]*>[a-zA-Z]|^<p[^>]*><b/) == 0 and not description_index
+          i += 1
+        end
+        process ".infobox img", :image=>"@src"
+
+        result  :image, :description
+      end
+
+      article = wiki_article.scrape(URI.parse("http://en.wikipedia.org/wiki/#{params['term']}"))
+      puts description_index
+      json = {:description => "", :image => ""}
+      json[:description] = article.description[description_index]  if article.description and article.description.size > 0
+      json[:image] = article.image[0] if article.image and article.image.size > 0
+
+      render :json => json
+    rescue
+      render :status => 400, :text => '-'# :nothing => true
+    end
+    render :text => wiki_article.scrape(URI.parse("http://en.wikipedia.org/wiki/Autotroph"))
   end
 
   private
