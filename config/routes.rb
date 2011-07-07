3a9c6a8ccbfc49c43601750dff12bc6434fa297b
Josh Silverman <joshs.silverman@gmail.com>
2011-07-07 12:01:07 -0400

    move lookup to new controller

--- config/routes.rb
+++ config/routes.rb
@@ -75,11 +75,13 @@ Zendo::Application.routes.draw do
   match "/documents/update_tag"
   match "/documents/share"
   match "/documents/unshare"
-  match "/documents/lookup/:term" => "documents#lookup"
   match "/documents/update_privacy"
   match "/documents/update_document_name"
   match "/documents/:id" => "documents#edit", :via => [:get], :read_only => true
   resources :documents, :only => [:edit, :update, :destroy]
+
+  # terms
+  match "/terms/lookup/:term" => "terms#lookup"
 
   # reviewer
   match "/review/:id" => "documents#review" #** public **#
