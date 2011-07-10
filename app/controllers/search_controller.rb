0488e39db5d829a21f005d9ed5bd67ebc566197c
Bill DeRusha <Bill.DeRusha@gmail.com>
2011-07-10 17:26:45 -0400

    pub search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,7 +5,7 @@ class SearchController < ApplicationController
   end
 
   def query
-    query = Document.select(['name', 'id']).where("name LIKE ? AND public", params[:q]+'%').to_json()
+    query = Document.select(['name', 'id']).where("name LIKE ? AND public", '%'+params[:q]+'%').to_json()
     render :text => query
   end
 end
