6a12527f328787b3e68dfd6929619f6e42779929
Bill DeRusha <Bill.DeRusha@gmail.com>
2011-11-10 19:33:15 -0500

    added resource request to demo page as a partial

--- config/routes.rb
+++ config/routes.rb
@@ -122,6 +122,7 @@ Zendo::Application.routes.draw do
   match "/user" => "tags#index"
   match "/users/autocomplete"
   match "/users/retrieve_notifications"
+  match "/users/add_resource_request"
   match "/users/add_device/:token" => "users#add_device"
   root :to => "users#home"
   root :controller => 'users', :action => 'home'
