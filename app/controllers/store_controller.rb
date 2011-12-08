6b73b7986c2e31833033192bc5fe309090d62c1e
Bill DeRusha <Bill.DeRusha@gmail.com>
2011-12-08 12:09:18 -0500

    pulls egg image from each individual tag

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,7 +1,7 @@
 class StoreController < ApplicationController
   def index
     egg_ids = [1927, 1928, 1814, 1815, 1828, 1830]
-    #egg_ids = [1,2,3,4,5] #hardcode for localtesting 
+    egg_ids = [1,2,3,4,5] #hardcode for localtesting
     @recent_public_eggs = Tag.where(:id => egg_ids)
     #@recent_public_eggs = Tag.joins(:documents).where("documents.public").group('tags.id').order('documents.updated_at desc').limit(9)
     @egg_prices = Hash.new
